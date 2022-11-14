571495335bf2d3e7d406fa308f2396ae68886919
Code Sandbox <hello@codesandbox.io>
2022-11-14 16:58:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ddc58461af44be6545aa164b45a543ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80422cb759ff707952291bcee69a7eea",
+    "url": "https://aratiadhikari99.github.io/csb-t3oivg/index.html"
+  },
+  {
+    "revision": "07a5a1e9956efdaa9fc6",
+    "url": "https://aratiadhikari99.github.io/csb-t3oivg/static/js/2.83133fa3.chunk.js"
+  },
+  {
+    "revision": "cc337ad5dfcc2b6c550c",
+    "url": "https://aratiadhikari99.github.io/csb-t3oivg/static/js/main.8caaee12.chunk.js"
+  },
+  {
+    "revision": "07376d92a8a170021a70",
+    "url": "https://aratiadhikari99.github.io/csb-t3oivg/static/js/runtime-main.641ef559.js"
+  }
+]);
